083358134e348fb78cafc50d58a540c0ba6fda66
Martin Ertsaas <mertsas@cisco.com>
2013-08-21 05:10:20 +0200

    For some reason the build directory is eglibc-eglibc

--- oebb.sh
+++ oebb.sh
@@ -133,7 +133,7 @@ else
     # Specify the root directory for your OpenEmbedded development
     #--------------------------------------------------------------------------
     OE_BUILD_DIR=${OE_BASE}
-    OE_BUILD_TMPDIR="${OE_BUILD_DIR}/build/tmp-${DISTRO_DIRNAME}-eglibc"
+    OE_BUILD_TMPDIR="${OE_BUILD_DIR}/build/tmp-${DISTRO_DIRNAME}-eglibc-eglibc"
     OE_SOURCE_DIR=${OE_BASE}/sources
     OE_LAYERS_TXT="${OE_SOURCE_DIR}/layers.txt"
 
